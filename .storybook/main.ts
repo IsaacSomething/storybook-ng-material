efc65b23a02708ecbdcfd9e8fa0f42446c468b5c
tim <cuthbert.tim@hotmail.com>
2023-06-08 10:08:34 +0200

    ^ storybook 7.0.20

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,14 @@
 import type { StorybookConfig } from '@storybook/angular';
 const config: StorybookConfig = {
   stories: ['../src/stories/**/*.mdx', '../src/stories/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    {
-      name: '@storybook/addon-styling',
-      options: {
-        // Check out https://github.com/storybookjs/addon-styling/blob/main/docs/api.md
-        // For more details on this addon's options.
-        postCss: true
-      }
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', {
+    name: '@storybook/addon-styling',
+    options: {
+      // Check out https://github.com/storybookjs/addon-styling/blob/main/docs/api.md
+      // For more details on this addon's options.
+      postCss: true
     }
-  ],
+  }, '@storybook/addon-mdx-gfm'],
   framework: {
     name: '@storybook/angular',
     options: {}
