89bd43ee2efa46e5fdf9b2f30a1cb1b24e5202eb
tim <cuthbert.tim@hotmail.com>
2023-06-08 11:32:47 +0200

    + menu

---
+++ src/stories/components/menu/menu.component.ts
@@ -0,0 +1,52 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { SBHeaderModule } from '../../assets/sb-header';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+
+@Component({
+  selector: 'app-menu',
+  standalone: true,
+  imports: [CommonModule, SBHeaderModule, MatMenuModule, MatButtonModule],
+  template: `
+    <sb-header title="Menu" matLink="menu" />
+
+    <button mat-flat-button color="primary" [matMenuTriggerFor]="menu">[Menu]</button>
+    <mat-menu #menu="matMenu" [yPosition]="yPosition" [xPosition]="xPosition" [hasBackdrop]="hasBackdrop" [overlapTrigger]="overlapTrigger">
+      <button mat-menu-item>Level 1a</button>
+      <button mat-menu-item>Level 2b</button>
+      <button mat-menu-item [matMenuTriggerFor]="level2">Multi level</button>
+    </mat-menu>
+
+    <mat-menu
+      #level2="matMenu"
+      [yPosition]="yPosition"
+      [xPosition]="xPosition"
+      [hasBackdrop]="hasBackdrop"
+      [overlapTrigger]="overlapTrigger"
+    >
+      <button mat-menu-item>Level 2a</button>
+      <button mat-menu-item>Level 2b</button>
+      <button mat-menu-item [matMenuTriggerFor]="level3">Level 2c</button>
+    </mat-menu>
+
+    <mat-menu
+      #level3="matMenu"
+      [yPosition]="yPosition"
+      [xPosition]="xPosition"
+      [hasBackdrop]="hasBackdrop"
+      [overlapTrigger]="overlapTrigger"
+    >
+      <button mat-menu-item>Level 3a</button>
+      <button mat-menu-item>Level 3b</button>
+      <button mat-menu-item>Level 3c</button>
+    </mat-menu>
+  `,
+  styles: []
+})
+export class MenuModule {
+  @Input() yPosition!: 'above' | 'below';
+  @Input() xPosition!: 'before' | 'after';
+  @Input() hasBackdrop!: boolean;
+  @Input() overlapTrigger!: boolean;
+}
---
+++ src/stories/components/menu/menu.stories.ts
@@ -0,0 +1,41 @@
+import { ArgTypes, Meta, StoryObj, applicationConfig } from '@storybook/angular';
+import { MenuModule } from './menu.component';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+const argTypes: ArgTypes<MenuModule> = {
+  yPosition: {
+    options: ['above', 'below'],
+    control: { type: 'radio' },
+    name: 'Y position'
+  },
+  xPosition: {
+    options: ['before', 'after'],
+    control: { type: 'radio' },
+    name: 'X position'
+  },
+  hasBackdrop: { control: { type: 'boolean' }, name: 'Has backdrop' },
+  overlapTrigger: { control: { type: 'boolean' }, name: 'Overlap trigger' }
+};
+
+const args: Partial<MenuModule> = {
+  yPosition: 'below',
+  xPosition: 'after',
+  hasBackdrop: true,
+  overlapTrigger: false
+};
+
+const meta: Meta<MenuModule> = {
+  title: 'Components/Menu',
+  component: MenuModule,
+  parameters: { options: { showPanel: true } },
+  decorators: [applicationConfig({ providers: [importProvidersFrom(BrowserAnimationsModule)] })],
+  argTypes
+};
+
+export default meta;
+type Story = StoryObj<MenuModule>;
+
+export const Menu: Story = {
+  args
+};
