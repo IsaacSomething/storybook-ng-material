e3f5a2b75b58f8b7f8e641b810d486aa894f8ca5
Timothy <tcuthbert@dvt.co.za>
2023-06-09 17:00:18 +0200

    + form field

--- src/stories/forms/form-field/form-field.component.ts
+++ src/stories/forms/form-field/form-field.component.ts
@@ -1,16 +1,35 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { Component, Input } from '@angular/core';
+import { FloatLabelType, MatFormFieldAppearance, MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { SBHeaderModule } from '../../assets/sb-header';
+import { MatIconModule } from '@angular/material/icon';
+import { ThemePalette } from '@angular/material/core';
 
 @Component({
   selector: 'app-form-field',
   standalone: true,
-  imports: [CommonModule, MatFormFieldModule, MatInputModule, MatAutocompleteModule, ReactiveFormsModule, SBHeaderModule],
-  template: ` <sb-header title="Form Field" matLink="form-field" /> `,
+  imports: [CommonModule, MatFormFieldModule, MatInputModule, SBHeaderModule, MatIconModule],
+  template: `
+    <sb-header title="Form Field" matLink="components/form-field/api" />
+
+    <div class="mx-5">
+      <mat-form-field [appearance]="appearance" [color]="color" [floatLabel]="floatLabel">
+        <mat-label>{{ label }}</mat-label>
+        <input matInput />
+        <mat-icon *ngIf="icon" matSuffix>bolt</mat-icon>
+        <mat-hint [align]="hintPosition">{{ hint }}</mat-hint>
+      </mat-form-field>
+    </div>
+  `,
   styles: []
 })
-export class FormFieldModule {}
+export class FormFieldModule {
+  @Input() label!: string;
+  @Input() hint!: string;
+  @Input() hintPosition!: 'start' | 'end';
+  @Input() icon!: boolean;
+  @Input() appearance!: MatFormFieldAppearance;
+  @Input() color!: ThemePalette;
+  @Input() floatLabel!: FloatLabelType;
+}
--- src/stories/forms/form-field/form-field.stories.ts
+++ src/stories/forms/form-field/form-field.stories.ts
@@ -3,9 +3,25 @@ import { FormFieldModule } from './form-field.component';
 import { importProvidersFrom } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-const argTypes: ArgTypes<FormFieldModule> = {};
+const argTypes: ArgTypes<FormFieldModule> = {
+  color: { options: ['primary', 'accent', 'warn'], control: { type: 'radio' }, name: 'Color' },
+  label: { control: { type: 'text' }, name: 'Label' },
+  hint: { control: { type: 'text' }, name: 'Label' },
+  hintPosition: { options: ['start', 'end'], control: { type: 'radio' }, name: 'Hint position' },
+  icon: { control: { type: 'boolean' }, name: 'Icon' },
+  appearance: { options: ['outline', 'fill'], control: { type: 'radio' }, name: 'Appearance' },
+  floatLabel: { options: ['auto', 'always', 'never'], control: { type: 'radio' }, name: 'Float label' }
+};
 
-const args: Partial<FormFieldModule> = {};
+const args: Partial<FormFieldModule> = {
+  label: 'Label',
+  hint: 'Hint',
+  hintPosition: 'start',
+  icon: false,
+  appearance: 'fill',
+  color: 'primary',
+  floatLabel: 'auto'
+};
 
 const meta: Meta<FormFieldModule> = {
   title: 'Forms/Form Field',
@@ -15,9 +31,6 @@ const meta: Meta<FormFieldModule> = {
   argTypes
 };
 
-export default meta;
 type Story = StoryObj<FormFieldModule>;
-
-export const FormField: Story = {
-  /* args */
-};
+export default meta;
+export const FormField: Story = { args };
